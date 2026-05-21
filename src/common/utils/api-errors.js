7e34d27f3ac17d0724a88eb67fd591089b6a22a3
Sai kiran <saikiran29.u@gmail.com>
2026-05-21 07:52:26 +0530

    RESTFUL-FUL API prep

---
+++ src/common/utils/api-errors.js
@@ -0,0 +1,10 @@
+class ApiErrors extends Errors {
+  constructor(statusCode, message) {
+    super(message);
+    this.statusCode = statusCode;
+    this.isOperational = true;
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+export { ApiErrors };
